c25e1bd2a2ccae3af14c8e107a421f911bcc69d1
Николай <Kolya.Balancev@yandex.ru>
2021-10-02 11:23:52 +0300

    pr3 done

--- src/pr3/MovablePoint.java
+++ src/pr3/MovablePoint.java
@@ -1,5 +1,40 @@
 package pr3;
 
-public class MovablePoint {
+public class MovablePoint implements Moveable {
+    private int x;
+    private int y;
+    private int xSpeed;
+    private int ySpeed;
 
+    MovablePoint(int x, int y, int xSpeed, int ySpeed) {
+        this.x = x;
+        this.y = y;
+        this.xSpeed = xSpeed;
+        this.ySpeed = ySpeed;
+    }
+
+    @Override
+    public String toString() {
+        return "MovablePoint{" + "x=" + x + ", y=" + y + ", xSpeed=" + xSpeed + ", ySpeed=" + ySpeed + '}';
+    }
+
+    @Override
+    public void moveUp() {
+        this.y += ySpeed;
+    }
+
+    @Override
+    public void moveDown() {
+        this.y -= ySpeed;
+    }
+
+    @Override
+    public void moveLeft() {
+        this.x -= xSpeed;
+    }
+
+    @Override
+    public void moveRight() {
+        this.x += xSpeed;
+    }
 }
