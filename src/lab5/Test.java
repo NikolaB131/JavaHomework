6759bb37a957b8ec638805a2e4407844085ac9d3
Николай <Kolya.Balancev@yandex.ru>
2021-10-10 16:57:28 +0300

    lab5 started

---
+++ src/lab5/Test.java
@@ -0,0 +1,12 @@
+package lab5;
+
+import javax.swing.*;
+
+public class Test extends JFrame {
+    public Test() {
+        this.setVisible(true);
+    }
+
+    private JTextField textField1 = new JTextField("da");
+    private JButton button1 = new JButton("OK");
+}
